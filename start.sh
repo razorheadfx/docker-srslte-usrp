a10601013558879b8ce129a9908f3c44c7bfc520
razorheadfx <razorheadfx@users.noreply.github.com>
2017-06-26 16:40:46 +0200

    Add --net=host to allow access to MME

--- start.sh
+++ start.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-docker run -it --rm --privileged -v /dev/bus/usb:/dev/bus/usb -v $PWD/conf:/conf srs bash
+#requires --privileged to create the tun interface
+#requires access to the usb bus to get to the usb
+#requires --net=host to access the host network namespace to communicate with the mme and the ue tunnel interface
+docker run -it --rm --privileged --net=host -v /dev/bus/usb:/dev/bus/usb -v $PWD/conf:/conf srs bash
